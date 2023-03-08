4d47ca04df3f363b338c4a9a9f5abfde5c83f903
AleksiyFlekssiy <alex77y71@gmail.com>
2023-03-08 22:51:07 +0300

    Now changing parameter's name works as intended. Fixed its re-creation in end of map. Refactor code in addData() method.

--- src/Person.java
+++ src/Person.java
@@ -7,16 +7,13 @@ public class Person {
     private final Scanner input = new Scanner(System.in);
     private final LinkedHashMap<String, Object> data;
 
-    
-
     public Person(){
         data = new LinkedHashMap<>();
     }
     public void addData(){
-        boolean exit = false;
         String key;
         Object value;
-        while (!exit) {
+        while (true) {
             System.out.println("Введите имя параметра");
             key = input.nextLine();
             if (key.equals("Exit")) return;
@@ -34,11 +31,20 @@ public class Person {
                 System.out.println("Введите имя параметра");
                 String oldKey = input.nextLine();
                 if (data.containsKey(oldKey)) {
-                    Object value = data.get(oldKey);
-                    data.remove(oldKey);
                     System.out.println("Введите новое имя параметра");
                     String newKey = input.nextLine();
-                    data.put(newKey, value);
+                    int size = data.size();
+                    String[] keys = data.keySet().toArray(new String[size]);
+                    Object[] values = data.values().toArray(new Object[size]);
+                    data.clear();
+                    for (int i = 0; i < size; i++){
+                        if(keys[i].equals(oldKey)){
+                            keys[i] = newKey;
+                            data.put(keys[i], values[i]);
+                        }
+                        else data.put(keys[i], values[i]);
+
+                    }
                     System.out.println("Имя параметра было изменено с " + oldKey + " на " + newKey);
                 }
                 else System.out.println("Такого параметра не существует");
@@ -53,9 +59,7 @@ public class Person {
                 }
                 else System.out.println("Такого параметра не существует");
             }
-            case "Оба" -> {
-                System.out.println("Введите имя параметра");
-            }
+            case "Оба" -> System.out.println("Введите имя параметра");
         }
     }
 
